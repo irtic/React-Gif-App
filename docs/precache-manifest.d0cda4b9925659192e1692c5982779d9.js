4eb261b680060d6587194050adcfa89dffa40882
checho <sgmunoz89@gmail.com>
2020-07-05 19:40:22 -0400

    Docs agregado

---
+++ docs/precache-manifest.d0cda4b9925659192e1692c5982779d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4943e065352d56db39dea26142e770ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017308f324080267aa18",
+    "url": "/static/css/main.666a1468.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017308f324080267aa18",
+    "url": "/static/js/main.83883d1a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
